a024a2431ba64b87695776d64a5ed5ade85438ec
abduqodir <abduqodir.sattorov@gmail.com>
2021-09-15 20:08:33 +0500

    first commit

---
+++ assets/js/lang.js
@@ -0,0 +1,193 @@
+const langArr = {
+    "unit":{
+        'ru':"Канференсия",
+        'uz':'Conference',
+        'de':'Conf'
+    },
+    '1':{
+        'ru':"Дом",
+        'uz':'Bosh sahifa',
+        'de':'Hauptseite'
+    },
+    '2':{
+        'ru':"О нас",
+        'uz':'Biz haqimizda',
+        'de':'Über uns'
+    },
+    '3':{
+        'ru':"Разгаворшик",
+        'uz':'Ma\'ruzachilar',
+        'de':'Lautsprecher'
+    },
+    '4':{
+        'ru':"Повестка дня",
+        'uz':'Kun tartibi',
+        'de':'Agenda'
+    },
+    '5':{
+        'ru':"Место проведения",
+        'uz':'Konferensiya joyi',
+        'de':'Vernastaltungsort'
+    },
+    '6':{
+        'ru':"Галерея",
+        'uz':'Galereya',
+        'de':'Galerie'
+    },
+    '7':{
+        'ru':"Партнер",
+        'uz':'Hamkorlar',
+        'de':'Partner'
+    },
+    '9':{
+        'ru':"99999",
+        'uz':'Raqamli Texnologiyalar davrida Nemis tilini o\'qitish',
+        'de':'99999'
+    },
+    '10':{
+        'ru':"99999",
+        'uz':'8-9 Oktabr, Toshkent shahar, Amir Temur ko\'chasi 42-uy',
+        'de':'99999'
+    },
+    '11':{
+        'ru':"О конференции",
+        'uz':'Konferensiya haqida',
+        'de':'Über die Konferenz'
+    },
+    '12':{
+        'ru':"О конференции",
+        'uz':'Konferensiya haqida',
+        'de':'Über die Konferenz'
+    },
+    '13':{
+        'ru':"",
+        'uz':"Anjuman ta'limining barcha bosqichlarida nemis tilini uqitish jarayoni eng so'nggi tadqiqot tajribalari, shungindek, raqamli texnologiyalar soxasidagi ilg'or xorijiy va milliy tojribani jo'riy natijalarining o'zlari ichiga oladi",
+        'de':''
+    },
+    '14':{
+        'ru':"Где",
+        'uz':'Qayerda',
+        'de':'Woher'
+    },
+    '15':{
+        'ru':"",
+        'uz':"Toshkent shahar, Amir Temur ko'chasi 42-uy",
+        'de':''
+    },
+    '16':{
+        'ru':"Когда",
+        'uz':"Qachon",
+        'de':"Wann"
+    },
+    '17':{
+        'ru':"",
+        'uz':"Juma - Shanba, 8-9 Oktabr",
+        'de':""
+    },
+    '18':{
+        'ru':"Спикеры мероприятия",
+        'uz':"Tadbir ma'ruzachilari",
+        'de':""
+    },
+    '19':{
+        'ru':"Наши спикеры",
+        'uz':"Bizning ma'ruzachilar",
+        'de':""
+    },
+    '20':{
+        'ru':"",
+        'uz':"Theme-1",
+        'de':""
+    },
+    '21':{
+        'ru':"",
+        'uz':"Theme-2",
+        'de':""
+    },
+    '22':{
+        'ru':"",
+        'uz':"Theme-2",
+        'de':""
+    },
+    '23':{
+        'ru':"",
+        'uz':"Theme-3",
+        'de':""
+    },
+    '24':{
+        'ru':"",
+        'uz':"Theme-4",
+        'de':""
+    },
+    '25':{
+        'ru':"",
+        'uz':"Theme-5",
+        'de':""
+    },
+    '26':{
+        'ru':"Повестка дня",
+        'uz':'Kun tartibi',
+        'de':'Agenda'
+    },
+    '27':{
+        'ru':"Наш план действий",
+        'uz':"Bizning tadbirlar rejasi",
+        'de':""
+    },
+    '28':{
+        'ru':"Первый день",
+        'uz':"Birinchi kun",
+        'de':""
+    },
+    '29':{
+        'ru':"Второй день",
+        'uz':"Ikkinchi kun",
+        'de':""
+    },
+    '30':{
+        'ru':"",
+        'uz':"Konferensiya manzili",
+        'de':""
+    },
+    '31':{
+        'ru':"Информация об адресе конференции",
+        'uz':"Konferensiya manzili haqida ma'lumot",
+        'de':""
+    },
+    '32':{
+        'ru':"",
+        'uz':"Toshkent shahar, Amir Temur ko'chasi 42-uy",
+        'de':""
+    },
+    '33':{
+        'ru':"",
+        'uz':"Anjuman ta'limining barcha bosqichlarida nemis tilini uqitish jarayoni eng so'nggi tadqiqot tajribalari, shungindek, raqamli texnologiyalar soxasidagi ilg'or xorijiy va milliy tojribani jo'riy natijalarining o'zlari ichiga oladi",
+        'de':""
+    },
+    '34':{
+        'ru':"Галерея",
+        'uz':"Galereya",
+        'de':"Galerie"
+    },
+    '35':{
+        'ru':"",
+        'uz':"Bizning galereyamizda bo'lib o'tgan tadbirlarimizni ko'rishingiz mumkin",
+        'de':""
+    },
+    '36':{
+        'ru':"Партнер",
+        'uz':"Hamkorlar",
+        'de':"Partner"
+    },
+    
+}
+
+
+
+/*
+'14':{
+    'ru':"",
+    'uz':"",
+    'de':""
+},
+*/
