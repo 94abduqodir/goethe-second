368e8ba4145278f741cd465fa053f47510ef4847
abduqodir <abduqodir.sattorov@gmail.com>
2021-10-05 02:26:06 +0500

    qushildi

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -274,10 +274,139 @@ const langArr = {
         'uz':"Biz bilan bog'lanish",
         'de':"Kontakt"
     },
-    
-    
-}
+    '55':{
+        'ru':"Введение",
+        'uz':"Kirish qismi",
+        'de':"Grußworte"
+    },
+    '56':{
+        'ru':"Войти через Zoom",
+        'uz':"Zoom orqali kirish",
+        'de':"Mit Zoom"
+    },
+    '57':{
+        'ru':"Войти",
+        'uz':"Kirish",
+        'de':"Eintreten"
+    },
+    '58':{
+        'ru':"",
+        'uz':"Germanya Federativ Respublikasining O'zbekistondagi elchixonasi doimiy vakili",
+        'de':""
+    },
+    '59':{
+        'ru':"Министерство высшего и среднего специального образования Республики Узбекистан",
+        'uz':"O'zbekiston Respublikasi Oliy va o'rta maxsus ta'lim vazirligi",
+        'de':""
+    },
+    '60':{
+        'ru':"Агентства по популяризации изучения иностранных языков",
+        'uz':"Xorijiy tillarni o'rganishni ommalshtirish agentligi direktori",
+        'de':""
+    },
+    '61':{
+        'ru':"Министерство народного образования Республики Узбекистан",
+        'uz':"O'zbekiston Respublikasi Xalq ta'lim vazirligi",
+        'de':""
+    },
+    '62':{
+        'ru':"Лектор ДААД, Национальный университет Узбекистана",
+        'uz':"O'zbekiston Milliy universiteti, DAAD- Lektori",
+        'de':"DAAD-Lektor, Nationale Universität Usbekistans"
+    },
+    '63':{
+        'ru':"Директор Института Гёте в Узбекистане",
+        'uz':"O'zbekistondagi Gyote instituti direktori",
+        'de':""
+    },
+    '64':{
+        'ru':"Обзор цифрового формата экзамена TestDaF",
+        'uz':"TestDaF raqamli imtihon formatining yangi ko'rinishi",
+        'de':"Der digitale TestDaF im Überblick "
+    },
+    '65':{
+        'ru':"Требования урока немецкого языка – цифровизация",
+        'uz':"Nemis tili darslarida qoʼyiladigan talablar - Raqamlashtirish",
+        'de':"Herausforderungen des DaF-Unterrichts - die Digitalisierung"
+    },
+    '66':{
+        'ru':"Современные обучающие программы и цифровые технологии обучения для урока немецкого языка в дошкольном, начальном, среднем и высшем образовании",
+        'uz':"Bogʼcha-maktab-oliy taʼlim tizimida zamonaviy taʼlim dasturlari va raqamli texnoligiyalari",
+        'de':"Moderne Lehrprogramme und digitale Technologien für den Deutschunterricht im Elementar-, Primar-, Sekundarbereich sowie in der Hochschulbildung"
 
+    },
+    '67':{
+        'ru':"Международные стандарты в обучении иностранным языкам",
+        'uz':"Xorijiy tillarni kiritishda xalqaro standartlar",
+        'de':"Internationale Standards im Fremdsprachenunterricht"
+    },
+    '68':{
+        'ru':"Новая концепция обеспечения преемственности и непрерывности в процессе обучения иностранным языкам",
+        'uz':"Xorijiy tillarni oʼqitishda uzluksizlikni taʼminlash yangi kontseptsiyasi",
+        'de':"Ein neues Konzept zur Gewährleistung von Kontinuität im Fremdsprachenunterricht"
+    },
+
+    '69':{
+        'ru':"Секция 1",
+        'uz':"1-Bo'lim",
+        'de':"Sektion 1"
+    },
+    '70':{
+        'ru':"Войти через Zoom",
+        'uz':"Zoom orqali kirish",
+        'de':"Mit Zoom"
+    },
+    '71':{
+        'ru':"Войти",
+        'uz':"Kirish",
+        'de':"Eintreten"
+    },
+    '72':{
+        'ru':"Секция 2",
+        'uz':"2-Bo'lim",
+        'de':"Sektion 2"
+    },
+    '73':{
+        'ru':"Войти через Zoom",
+        'uz':"Zoom orqali kirish",
+        'de':"Mit Zoom"
+    },
+    '74':{
+        'ru':"Войти",
+        'uz':"Kirish",
+        'de':"Eintreten"
+    },
+    '75':{
+        'ru':"Секция 3",
+        'uz':"3-Bo'lim",
+        'de':"Sektion 3"
+    },
+    '76':{
+        'ru':"Войти через Zoom",
+        'uz':"Zoom orqali kirish",
+        'de':"Mit Zoom"
+    },
+    '77':{
+        'ru':"Войти",
+        'uz':"Kirish",
+        'de':"Eintreten"
+    },
+    '78':{
+        'ru':"Секция 4",
+        'uz':"4-Bo'lim",
+        'de':"Sektion 4"
+    },
+    '79':{
+        'ru':"Войти через Zoom",
+        'uz':"Zoom orqali kirish",
+        'de':"Mit Zoom"
+    },
+    '80':{
+        'ru':"Войти",
+        'uz':"Kirish",
+        'de':"Eintreten"
+    },
+}
 
 
 /*
